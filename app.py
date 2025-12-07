46212d644bdbffaacaf9623c7816f0de380d0369
bademasta <badb@purg.at>
2025-12-08 00:04:46 +0100

    changed all the labels and buttons to use container

--- app.py
+++ app.py
@@ -3,18 +3,25 @@ from textual.app import App , ComposeResult , Screen
 from textual import events
 from textual.containers import Container
 from textual.widgets import  Button, Label
+import psutil
 
 """The App"""
 class MyApp(App) :
 
+    """CSS Pfad"""
     CSS_PATH = "stylish.css"
 
     def compose(self) -> ComposeResult:
-        yield  Label("Hello Textual", id="label1")
-        yield  Button("Click hier !", id="click1")
-        yield  Button("Click NICHT hier !", id="click2")
-        #with Container():
-        #    yield  Label("Was wirst du drücken????", id="label2")
+
+        with Container():
+            yield Label("Was wirst du drücken????", id="label2")
+            yield Button("Click hier !", id="click1")
+            yield Button("Click NICHT hier !", id="click2")
+            yield Label("Q um zu beenden", id="label1")
+
+    def on_mount(self):
+        self.set_interval(1.0, self.batch_update )
+    
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         if event.button.id == "click1":
@@ -33,4 +40,6 @@ if __name__ == "__main__":
     app.run()
 
 
-
+# fetching and displaying of system processes with putil
+# ask for user input , and give the user the ability to write in a promt box to start fetures
+# implement 
